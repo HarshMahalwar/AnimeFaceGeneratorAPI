537f22c2b8e57a73886fae95f8664fc1ccd5b2cb
Harsh Mahalwar <78298221+HarshMahalwar@users.noreply.github.com>
2023-02-12 11:54:14 +0530

    Updateddd

--- DeployGAN/settings.py
+++ DeployGAN/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-01c4z*re@7-g+ks(&8sara6@@rkd)^bg$mu+t2@2osfy16q@+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
